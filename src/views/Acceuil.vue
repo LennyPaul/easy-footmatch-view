364d6b8adbff762499a93f32581ba11155510c4e
lennypaul <lenny@bobochicparis.com>
2023-04-20 16:56:06 +0200

    first commit

---
+++ src/views/Acceuil.vue
@@ -0,0 +1,37 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>FOOTBALL VIEWER</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true"  class="ion-padding">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">FOOTBALL VIEWER</ion-title>
+        </ion-toolbar>
+          <ion-list :inset="true">
+              <ion-item href="/ligue/4334">
+                  <ion-label>Ligue 1</ion-label>
+              </ion-item>
+              <ion-item href="/ligue/4401">
+                  <ion-label>Ligue 2</ion-label>
+              </ion-item>
+              <ion-item href="/ligue/4637">
+                  <ion-label>National 1</ion-label>
+              </ion-item>
+          </ion-list>
+      </ion-header>
+
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonList } from '@ionic/vue';
+import { defineComponent } from 'vue';
+export default defineComponent({
+    components: { IonItem, IonLabel, IonList },
+});
+
+</script>
--- src/views/ligue.vue
+++ src/views/ligue.vue
@@ -2,22 +2,34 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Tab 2</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
+          <ion-nav :root="component"></ion-nav>
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Tab 2</ion-title>
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Tab 1 page" />
+      <ExploreContainer name="Tab 2 page" />
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonNav } from '@ionic/vue';
+import PageOne from './Acceuil.vue';
+
+export default {
+    components: { IonNav },
+    data() {
+        return {
+            component: PageOne,
+        };
+    },
+};
 </script>
